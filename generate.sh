c3b8e89af351d24663c3153c057fea839a000a87
Madhur Ahuja <ahuja.madhur@gmail.com>
2024-11-15 23:38:45 +0530

    feat: generate wallpapers

---
+++ generate.sh
@@ -0,0 +1,6 @@
+cp -r ~/Pictures/wallpapers/day .
+cp -r ~/Pictures/wallpapers/morning .
+cp -r ~/Pictures/wallpapers/night .
+cp -r ~/Pictures/wallpapers/midnight .
+cp -r ~/Pictures/wallpapers/evening .
+cp -r ~/Pictures/wallpapers/vertical .
